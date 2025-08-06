109f650c765c94d665b82c16d6e7802fbfeaa3c2
dawens8 <mydawens8@gmail.com>
2025-08-06 11:08:02 -0400

    Update auto-reply.js

--- plugins/auto-reply.js
+++ plugins/auto-reply.js
@@ -9,7 +9,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../media/autoreply.json');
+    const filePath = path.join(__dirname, '../all/media/autoreply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
